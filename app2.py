53e052c396cf343629db5b47b4b9e51bfa70faa2
Teresa Bui <t3resabui@ucla.edu>
2023-06-14 15:16:31 -0700

    app2.py

--- app2.py
+++ app2.py
@@ -1,73 +1,66 @@
 import streamlit as st
 import pandas as pd
 from matplotlib import pyplot as plt
-import io
+import seaborn as sns
 
-web_apps = st.sidebar.selectbox("Select Web Apps",
-                                ("Exploratory Data Analysis", "Distributions"))
+web_apps = st.sidebar.selectbox("Select Web Apps", ("Exploratory Data Analysis", "Distributions"))
 
 df = None
 
 if web_apps == "Exploratory Data Analysis":
+    uploaded_file = st.sidebar.file_uploader("Choose a file")
 
-  uploaded_file = st.sidebar.file_uploader("Choose a file")
+    if uploaded_file is not None:
+        # Can be used wherever a "file-like" object is accepted:
+        df = pd.read_csv(uploaded_file)
+        show_df = st.checkbox("Show Data Frame", key="disabled")
 
-  if uploaded_file is not None:
-    # Can be used wherever a "file-like" object is accepted:
-    df = pd.read_csv(uploaded_file)
-    show_df = st.checkbox("Show Data Frame", key="disabled")
+        if show_df:
+            st.write(df)
 
-    if show_df:
-      st.write(df)
+        num_rows = df.shape[0]
+        num_columns = df.shape[1]
+        num_categorical = len(df.select_dtypes(include=['object']).columns)
+        num_numerical = len(df.select_dtypes(include=['int64', 'float64']).columns)
+        num_boolean = len(df.select_dtypes(include=['bool']).columns)
 
-    num_rows = df.shape[0]
-    num_columns = df.shape[1]
-    num_categorical = len(df.select_dtypes(include=['object'].columns))
-    num_numerical = len(df.select_dtypes(include=['int64', 'float64']).columns)
-    num_boolean = len(df.select_dtypes(include=['bool']).columns)
+        st.subheader("Dataset Statistic")
+        st.write("Number of rows:", num_rows)
+        st.write("Number of columns:", num_columns)
+        st.write("Number of categorical variables:", num_categorical)
+        st.write("Number of numerical variables:", num_numerical)
+        st.write("Number of boolean variables:", num_boolean)
 
-    st.subheader("Dataset Statistic")
-    st.write("Number of rows:", num_rows)
-    st.write("Number of columns:", num_columns)
-    st.write("Number of categorical variables:", num_categorical)
-    st.write("Number of numerical variables:", num_numerical)
-    st.write("Number of boolean variables:", num_boolean)
+        selected_column = st.sidebar.selectbox("Select a Column", df.columns)
 
-    selected_column = st.sidebar.selectbox("Select a Column", df.columns)
+        column_type = st.sidebar.selectbox('Select Data Type', ("Numerical", "Categorical", "Bool", "Date"))
 
-    column_type = st.sidebar.selectbox('Select Data Type',
-                                       ("Numerical", "Categorical", "Bool", "Date"))
+        if column_type == "Numerical":
+            numerical_column = st.sidebar.selectbox('Select a Column', df.select_dtypes(include=['int64', 'float64']).columns)
 
-    if column_type == "Numerical":
-      numerical_column = st.sidebar.selectbox(
-          'Select a Column', df.select_dtypes(include=['int64', 'float64']).columns)
+            # histogram
+            choose_color = st.color_picker('Pick a Color', "#69b3a2")
+            choose_opacity = st.slider('Color Opacity', min_value=0.0, max_value=1.0, step=0.05)
 
-      # histogram
-      choose_color = st.color_picker('Pick a Color', "#69b3a2")
-      choose_opacity = st.slider(
-          'Color Opacity', min_value=0.0, max_value=1.0, step=0.05)
+            hist_bins = st.slider('Number of bins', min_value=5, max_value=150, value=30)
+            hist_title = st.text_input('Set Title', 'Histogram')
+            hist_xtitle = st.text_input('Set x-axis Title', numerical_column)
 
-      hist_bins = st.slider('Number of bins', min_value=5,
-                            max_value=150, value=30)
-      hist_title = st.text_input('Set Title', 'Histogram')
-      hist_xtitle = st.text_input('Set x-axis Title', numerical_column)
+            fig, ax = plt.subplots()
+            ax.hist(df[numerical_column], bins=hist_bins, edgecolor="black", color=choose_color, alpha=choose_opacity)
+            ax.set_title(hist_title)
+            ax.set_xlabel(hist_xtitle)
+            ax.set_ylabel('Count')
 
-      fig, ax = plt.subplots()
-      ax.hist(df[numerical_column], bins=hist_bins,
-              edgecolor="black", color=choose_color, alpha=choose_opacity)
-      ax.set_title(hist_title)
-      ax.set_xlabel(hist_xtitle)
-      ax.set_ylabel('Count')
+            st.pyplot(fig)
+            filename = "plot.png"
+            fig.savefig(filename, dpi=300)
 
-      st.pyplot(fig)
-      filename = "plot.png"
-      fig.savefig(filename,dpi = 300)
-
-      # Display the download button
-      with open("plot.png", "rb") as file:
-        btn = st.download_button(
-            label="Download image",
-            data=file,
-            file_name="flower.png",
-            mime="image/png"
-        )
+            # Display the download button
+            with open("plot.png", "rb") as file:
+                btn = st.download_button(
+                    label="Download image",
+                    data=file,
+                    file_name="flower.png",
+                    mime="image/png"
+                )
